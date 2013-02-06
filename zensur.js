6b2303202376c6d266d6a88ae74e7f96a8a331ad
Adrian Friedli <adi@koalatux.ch>
2013-02-06 19:29:47 +0100

    also censor single words

--- zensur.js
+++ zensur.js
@@ -4,9 +4,15 @@ var zensur = {
   // 0: uncensored0 -> 1: uncensored1 -> 2: censored -> 0: uncensored0
   // state transition probabilities:
   p_trans: [
+    // 0     uncensored0 -> uncensored0
     // 1     uncensored0 -> uncensored1
-    0.12, // uncensored1 -> censored
-    0.8,  // censored -> uncensored0
+    // 0     uncensored0 -> censored
+    0.02, // uncensored1 -> uncensored0
+    // x     uncensored1 -> uncensored1
+    0.08, // uncensored1 -> censored
+    0.7,  // censored    -> uncensored0
+    // 0     censored    -> uncensored1
+    // x     censored    -> censored
   ],
   start_censor: "<span class=\"censored\">",
   stop_censor: "</span>",
@@ -16,13 +22,23 @@ var zensur = {
       return word;
     }
     if (state.s === 0) {
+      // uncensored0 -> uncensored1
       state.s = 1;
+      return word;
     }
-    else if (state.s === 1 && Math.random() < this.p_trans[0]) {
+    var r = Math.random();
+    if (state.s === 1 && r < this.p_trans[0]) {
+      // uncensored1 -> uncensored0
+      state.s = 2;
+      return this.start_censor + word + this.stop_censor;
+    }
+    else if (state.s === 1 && r < this.p_trans[0] + this.p_trans[1]) {
+      // uncensored1 -> censored
       state.s = 2;
       return this.start_censor + word;
     }
-    else if (state.s === 2 && Math.random() < this.p_trans[1]) {
+    else if (state.s === 2 && r < this.p_trans[2]) {
+      // censored -> uncensored0
       state.s = 0;
       return word + this.stop_censor;
     }
