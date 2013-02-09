457410d59d3a810fe0505516576b156dd7c7769b
Adrian Friedli <adi@koalatux.ch>
2013-02-09 14:09:40 +0100

    scope function instead of object

--- zensur.js
+++ zensur.js
@@ -1,11 +1,10 @@
-var zensur = {
-
+function zensurjs() {
   // states
   // 0: uncensored
   // 1: start_censor
   // 2: censored
   // state transition probabilities:
-  p_trans: [
+  var p_trans = [
     // x     uncensored   -> uncensored
     0.08, // uncensored   -> start_censor
     // 0     uncensored   -> censored
@@ -15,14 +14,14 @@ var zensur = {
     0.55, // censored   -> uncensored
     // 0     censored   -> start_censor
     // x     censored   -> censored
-  ],
-  start_censor: "<span class=\"censored\">",
-  stop_censor: "</span>",
-  re_censor: /[0-9A-Za-z]/,
+  ];
+  var start_censor = "<span class=\"censored\">";
+  var stop_censor = "</span>";
+  var re_censor = /[0-9A-Za-z]/;
 
-  censorWord: function(state, word) {
+  function censorWord(state, word) {
     // ignore words not containing letters (may be spaces only)
-    if (!word.match(this.re_censor)) {
+    if (!word.match(re_censor)) {
       return word;
     }
 
@@ -33,41 +32,41 @@ var zensur = {
     if (state.s === 1) {
       // start_censor -> censored
       state.s = 2;
-      prefix = this.start_censor;
+      prefix = start_censor;
     }
 
-    if (state.s === 0 && r < this.p_trans[0]) {
+    if (state.s === 0 && r < p_trans[0]) {
       // uncensored -> start_censor
       state.s = 1;
     }
-    else if (state.s === 2 && r < this.p_trans[1]) {
+    else if (state.s === 2 && r < p_trans[1]) {
       // censored -> uncensored
       state.s = 0;
-      suffix = this.stop_censor;
+      suffix = stop_censor;
     }
 
     return prefix + word + suffix;
-  },
+  }
 
-  censorTextNode: function(node) {
+  function censorTextNode(node) {
     var state = { s: 0 };
-    if (Math.random() < this.p_trans[0]) {
+    if (Math.random() < p_trans[0]) {
       // start_censor
       state.s = 1;
     }
     var words = node.nodeValue.split(/([\t\n\r ])/);
     for (var i in words) {
-      words[i] = this.censorWord(state, words[i]);
+      words[i] = censorWord(state, words[i]);
     }
     if (state.s === 2) {
-      words[words.length-1] = words[words.length-1] + this.stop_censor;
+      words[words.length-1] = words[words.length-1] + stop_censor;
     }
     var newnode = document.createElement("span");
     newnode.innerHTML = words.join("");
     return newnode;
-  },
+  }
 
-  censorElement: function(element) {
+  function censorElement(element) {
     var childs = element.childNodes;
     for (var i in childs) {
       if (childs[i].nodeType === 1 &&
@@ -78,14 +77,15 @@ var zensur = {
           childs[i].nodeName !== "TEXTAREA" &&
           childs[i].nodeName !== "TITLE")
       {
-        this.censorElement(childs[i]);
+        censorElement(childs[i]);
       }
       else if (childs[i].nodeType === 3 &&
-          childs[i].nodeValue.match(this.re_censor))
+          childs[i].nodeValue.match(re_censor))
       {
-        element.replaceChild(this.censorTextNode(childs[i]), childs[i]);
+        element.replaceChild(censorTextNode(childs[i]), childs[i]);
       }
     }
-  },
+  }
 
-};
+  return censorElement;
+}
